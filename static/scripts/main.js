e247581e1b1038b0d996b70adfc713feb4815197
MuteenK <kundangarmuteen@protonmail.com>
2023-02-11 17:19:22 +0530

    UI Home and Analyze page

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,25 @@
-console.log("Hello")
+// Grabbing Elements
+const hamBtn = document.querySelector("#ham-btn");
+const menu = document.querySelector("#menu");
+const closeBtn = document.querySelector("#close-btn");
+
+// Adding Event Listener
+hamBtn.addEventListener("click", openMenu)
+closeBtn.addEventListener("click", closeMenu)
+
+// Functions
+function openMenu() {
+    if (menu.classList.contains("hide")) {
+        menu.classList.toggle("hide");
+    }
+    // console.log(menu.classList)
+    menu.classList.toggle("show");
+}
+
+function closeMenu() {
+    if (menu.classList.contains("show")) {
+        menu.classList.toggle("show");
+    }
+    // console.log(menu.classList)
+    menu.classList.toggle("hide");
+}
